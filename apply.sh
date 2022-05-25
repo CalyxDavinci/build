83ec4a0effb8ce36285cdaad82fe83e26c905e73
Jens Reidel <adrian@travitia.xyz>
2022-05-25 14:35:59 +0200

    Add bromite patch

--- apply.sh
+++ apply.sh
@@ -17,15 +17,20 @@ echo "[i] Fetching is done. Patching sources..."
 
 cd frameworks/base
 echo "[i] Applying 0001-core-jni-Switch-to-O3.patch"
-git am -3 $CATHEDRA_PATH/dcos/patches/0001-core-jni-Switch-to-O3.patch
+git am -3 $CATHEDRA_PATH/patches/0001-core-jni-Switch-to-O3.patch
 cd $BASE_BUILD_DIR
 
 cd build/soong
 echo "[i] Applying 0002-soong-clang-builds-with-O3.patch"
-git am -3 $CATHEDRA_PATH/dcos/patches/0002-soong-clang-builds-with-O3.patch
+git am -3 $CATHEDRA_PATH/patches/0002-soong-clang-builds-with-O3.patch
 cd $BASE_BUILD_DIR
 
 cd bionic
 echo "[i] Applying 0003-libc-switch-to-jemalloc-from-scudo.patch"
-git am -3 $CATHEDRA_PATH/dcos/patches/0003-libc-switch-to-jemalloc-from-scudo.patch
+git am -3 $CATHEDRA_PATH/patches/0003-libc-switch-to-jemalloc-from-scudo.patch
+cd $BASE_BUILD_DIR
+
+cd vendor/calyx
+echo "[i] Applying 0004-Use-bromite-instead-of-chromium-webview.patch"
+git am -3 $CATHEDRA_PATH/patches/0004-Use-bromite-instead-of-chromium-webview.patch
 cd $BASE_BUILD_DIR
