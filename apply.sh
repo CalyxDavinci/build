4220d340bb049d962c6f8a3d0bb485916d8d7bf5
Jens Reidel <adrian@travitia.xyz>
2022-08-22 14:59:25 +0200

    build: Add kernel clang

--- apply.sh
+++ apply.sh
@@ -4,12 +4,18 @@ SCRIPT_PATH=$(realpath $0)
 CATHEDRA_PATH=$(dirname $SCRIPT_PATH)
 BASE_BUILD_DIR=$(pwd)
 
-echo "[i] Fetching latest Adrian Clang build for the kernel build..."
+echo "[i] Fetching latest Adrian Clang build for the ROM build..."
+# adrian-clang is used for building AOSP
 rm -rf prebuilts/clang/host/linux-x86/adrian-clang
 mkdir -p prebuilts/clang/host/linux-x86/adrian-clang
 cd prebuilts/clang/host/linux-x86/adrian-clang
-# in case of automated builds, please host a mirror
 curl https://ftp.travitia.xyz/clang/clang-latest.tar.xz | tar -xJ
+cd $BASE_BUILD_DIR
+# adrian-clang-2 is used for building the kernel
+rm -rf prebuilts/clang/host/linux-x86/adrian-clang-2
+mkdir -p prebuilts/clang/host/linux-x86/adrian-clang-2
+cd prebuilts/clang/host/linux-x86/adrian-clang-2
+curl https://ftp.travitia.xyz/clang/clang-abce7acebd4c06c977bc4bd79170697f1122bc5e.tar.xz | tar -xJ
 cd $BASE_BUILD_DIR
 
 $CATHEDRA_PATH/apply.py $CATHEDRA_PATH/patches
